7a3e6992ef50eaba8fd622030ebed9ced810066b
Gloria Wang <gw4164@gmail.com>
2024-12-03 01:34:34 -0500

    Create 1_enhancer_identification.sh

---
+++ code/1_enhancer_identification.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# 1_enhancer_identification.sh
+# Purpose: Identify putative enhancers in K562 cells using ATAC-seq data
+# by excluding regions that overlap with promoters
+
+# Set working directory
+WORKDIR="/ihome/biosc1542_2024s/glw30/mp2"
+cd $WORKDIR
+
+# Load required modules
+module load gcc/8.2.0 bedtools/2.30.0
+
+# Define input files
+GENCODE="/ix1/biosc1542_2024s/data/hg38.gencode.bed12"
+CHR_SIZES="/ix1/biosc1542_2024s/data/hg38_chr_sizes.txt"
+ATAC_PEAKS="/ix1/biosc1542_2024s/mp2/k562_atac_open_summits.bed"
+
+# Create 4kb promoter regions centered on transcription start sites
+echo "Creating promoter regions..."
+bedtools flank -i ${GENCODE} \
+    -g ${CHR_SIZES} \
+    -l 2000 -r 0 -s | \
+    bedtools slop -i stdin \
+    -g ${CHR_SIZES} \
+    -l 0 -r 2000 -s > promoters.bed
+
+# Identify putative enhancers by excluding promoter regions
+echo "Identifying putative enhancers..."
+bedtools subtract -a ${ATAC_PEAKS} \
+    -b promoters.bed > k562_enh.bed
+
+# Count number of enhancers
+echo "Number of putative enhancers identified:"
+wc -l k562_enh.bed
