1ebb621098d850df3829b829325e4aedb3eee610
Gloria Wang <gw4164@gmail.com>
2024-12-05 00:08:43 -0500

    Update 1_enhancer_identification.sh

--- code/1_enhancer_identification.sh
+++ code/1_enhancer_identification.sh
@@ -9,7 +9,6 @@ GENCODE="hg38.gencode.bed12"
 CHR_SIZES="hg38_chr_sizes.txt"
 ATAC_PEAKS="k562_atac_open_summits.bed"
 
-echo "Creating promoter regions..."
 bedtools flank -i ${GENCODE} \
     -g ${CHR_SIZES} \
     -l 2000 -r 0 -s | \
@@ -17,9 +16,7 @@ bedtools flank -i ${GENCODE} \
     -g ${CHR_SIZES} \
     -l 0 -r 2000 -s > promoters.bed
 
-echo "Identifying putative enhancers..."
 bedtools subtract -a ${ATAC_PEAKS} \
     -b promoters.bed > k562_enh.bed
 
-echo "Number of putative enhancers identified:"
 wc -l k562_enh.bed
